6f932d2fc03cdf0f5b266e03250e30365a457b61
l3m1j053ph <lemikayanga08@gmail.com>
2023-08-30 11:40:09 +0300

    added to comments to explain the code

--- tables.sql
+++ tables.sql
@@ -4,16 +4,19 @@ CREATE TABLE student (
     age int,
     town VARCHAR(255) NOT NULL);
 
--- altering table
+-- altering the student table by chnaging "name" to "student_name"
 ALTER TABLE student
 RENAME COLUMN name to student_name;
 
+-- altering the student table by adding the gender column
 ALTER TABLE student
 ADD gender VARCHAR(255);
 
+-- altering the student table removing the gender column
 ALTER TABLE student
 DROP COLUMN gender;
 
+-- altering the student table by modifying the age column
 ALTER TABLE student
 MODIFY COLUMN age VARCHAR(255);
 
